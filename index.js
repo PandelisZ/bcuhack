206411e52835c14d10d6c1e08576a61808acb2ee
Pandelis Zembashis <info@pandelis.me>
2016-02-29 16:41:29 +0000

    ping endpoint

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ app.get('/', function(req, res){
   res.send('works');
 });
 
+app.get('/api/haveyoutweetedthislinkyet', function(req, res){
+  if(req.query.ping != null){
+    res.statusCode = 400;
+    res.send('All good bud.')
+  }else {
+    res.send('This endpoint doesnt do much else')
+  }
+});
+
 app.get('/api/danielisnotbraveenoughtousevim', function(req,res){
 
   apiDb.find().exec(function(err, data){
