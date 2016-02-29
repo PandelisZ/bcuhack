b6a39c9ddaab77a1f47ca2e8bc0e114272408f84
Pandelis Zembashis <info@pandelis.me>
2016-02-29 17:58:12 +0000

    req dbug

--- index.js
+++ index.js
@@ -6,8 +6,25 @@ var router = express.Router();
 var port = process.env.PORT || 8000;
 var apiDb = require('./app/models/api');
 
+var request = require('request');
+
+
 
 mongoose.connect('mongodb://bcuhack:bcuhack@ds019068.mlab.com:19068/bcuhack');
+
+function getMajestic(cmd, item0){
+
+  var majestic = 'http://developer.majestic.com/api/json?app_api_key=8AF4E8A4408F14234B3B1E3694BD8654&cmd='+cmd+'&items=1&item0='+item0+'&datasource=fresh'
+
+  request(majestic, function (error, response, body) {
+    console.log(body);
+    if (!error && response.statusCode == 200) {
+      return JSON.parse(body);
+    }
+  })
+
+}
+
 
 
 router.use(function(req, res, next){
@@ -23,6 +40,20 @@ app.get('/', function(req, res){
   res.send('works');
 });
 
+app.get('/api/geturl/:id', function(req,res){
+  var id = 'http://' + req.params.id;
+
+  console.log(id);
+
+  var sendback = getMajestic('GetIndexItemInfo', id);
+
+  consoe.log(sendback);
+
+  res.json()
+
+
+})
+
 app.get('/api/haveyoutweetedthislinkyet', function(req, res){
   if(req.query.ping != null){
     res.statusCode = 200;
@@ -33,6 +64,8 @@ app.get('/api/haveyoutweetedthislinkyet', function(req, res){
 });
 
 app.get('/api/danielisnotbraveenoughtousevim', function(req,res){
+
+  console.log(req);
 
   apiDb.find().exec(function(err, data){
     res.json(data);
